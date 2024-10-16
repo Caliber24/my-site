f4c1ce908f42e42194f9918994103de078ab0078
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-10-16 09:11:57 +0330

    create admin post and category

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Post
+from .models import Category
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    # empty_value_display = "-empty-"
+    # fields = ('title',)
+    exclude = ("counted_view",)
+    list_display = ("title",'author', "counted_view", "status", "published_date", "created_date",)
+    list_filter = ('status','author')
+    search_fields = ('title', 'content')
+    
+    
+admin.site.register(Category)
